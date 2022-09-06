77eee0b47e8d54737e7cc40f47deb6320ad48c01
Willian Sant Anna <w.s.santanna@gmail.com>
2022-09-06 12:32:45 -0300

    Implementa o código fonte da Aula 16

---
+++ aulas/16/mesa-de-trabalho/MyDgPlayground/src/App.jsx
@@ -0,0 +1,82 @@
+
+import { useEffect, useState } from "react";
+
+
+export default function App() {
+
+	const [artigos, definirArtigos] = useState([]);
+	const [comentarios, definirComentarios] = useState([]);
+	const [perfil, definirPerfil] = useState({});
+
+	useEffect(() => {
+
+		/* 
+	
+			Adicione a comunicação 
+			com servidor aqui...
+
+			Endpoint
+
+			GET http://localhost:3000/posts
+
+			GET http://localhost:3000/comments
+
+			GET http://localhost:3000/profile
+
+			Response
+
+			{
+				"posts": [
+					{ 
+						"id": 1, 
+						"title": "json-server", 
+						"author": "typicode" 
+					}
+				],
+				"comments": [
+					{ 
+						"id": 1, 
+						"body": "some comment", 
+						"postId": 1 
+					}
+				],
+				"profile": { 
+					"name": "typicode" 
+				}
+			}
+		
+		*/
+
+
+		fetch('http://localhost:3000/posts')
+			.then((response) => response.json())
+			.then((json) => definirArtigos(json));
+
+		fetch('http://localhost:3000/comments')
+			.then((response) => response.json())
+			.then((json) => definirComentarios(json));
+
+
+
+	}, []);
+
+
+	/*
+		Renderize as tarefas em tela...
+		utilize map()
+	*/
+	return (
+		<>
+			<div>
+				<h1>Publicações</h1>
+				{artigos.map((artigo) => <div key={artigo.id}>{artigo.title}</div>)}
+			</div>
+
+			<div>
+				<h1>Comentários</h1>
+				{comentarios.map((comentario) => <div key={comentario.id}>{comentario.body}</div>)}
+			</div>
+		</>
+
+	)
+}
